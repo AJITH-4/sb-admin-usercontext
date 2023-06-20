52362fcce76b8ba7c498bc09ede9aba41880eb8f
AJITH-4 <ajith04deva@gmail.com>
2023-06-20 08:53:57 +0530

    context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import Sidebar from "./component/Sidebar";
+import Dashboard from "./component/Dashboard";
+import AddUser from "./component/AddUser"
+import Profile from './component/Profile'
+import PendingRequest from './component/PendingRequest'
+import Task from "./component/Task";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
+import ProfileDetails from "./component/ProfileDetails";
+import ResetPassword from "./component/ResetPassword";
+import React from "react";
+import UserContextComponent from "./context/UserContextComponent"
+import DashboardContextComponent from "./context/DashboardContextComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <>
+  <BrowserRouter>
+    <div id="wrapper">
+        <Sidebar/>
+        <Routes>
+          <Route path="/dashboard" element={
+            <DashboardContextComponent>
+              <UserContextComponent>
+                <Dashboard/>
+              </UserContextComponent>
+            </DashboardContextComponent>
+            }/>
+            
+          <Route path="/add-user" element={
+            <UserContextComponent>
+              <AddUser/>
+            </UserContextComponent>}/>
+          <Route path="/edit-user/:id" element={
+            <UserContextComponent>
+              <AddUser/>
+            </UserContextComponent>}/>
+          <Route path="/profile" element={<Profile/>}>
+              <Route path='details' element={<ProfileDetails/>}/>
+              <Route path='reset-password' element={<ResetPassword/>}/>
+          </Route>
+          <Route path="/pending-request" element={<PendingRequest/>}/>
+          <Route path="/task" element={<Task/>}/>
+          <Route path="*" element={<Navigate to='/dashboard'/>}/>
+        </Routes>
     </div>
-  );
+  </BrowserRouter>
+  </>
 }
 
 export default App;
